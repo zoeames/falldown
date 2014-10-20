e9cc9fc0aec877d2f14a7bd9d834bd13d103f949
Dave Boling <kadowki@gmail.com>
2014-10-20 13:29:03 -0500

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,11 @@
+angular.module('starter', ['ionic'])
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
